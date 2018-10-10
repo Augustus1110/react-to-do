0a75338350844f5d85f691ac6e0a535fb2c2d087
Donahi Casas-Silva <DCS@Donahis-MacBook-Pro.local>
2018-10-09 17:44:42 -0700

    Checkpoint 08 React to Do Events: Completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 class ToDo extends Component {
   render() {
     return (
-      <li>
-         <input type="checkbox" checked={ this.props.isCompleted } />
+      <div>
+       <li>{ this.props.description }</li>
+       <li>
+         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
        </li>
+       </div>
     );
   }
 }
