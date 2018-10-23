c0f60afb75a092589af25b7e4d6890e127d6eea6
Donahi Casas-Silva <DCS@Donahis-MacBook-Pro.local>
2018-10-23 09:45:29 -0700

    Assignment 08 completed

--- src/App.js
+++ src/App.js
@@ -33,12 +33,16 @@ class App extends Component {
           this.setState({ todos: todos });
         }
 
+  deleteTodo(index) {
+    this.setState({ todos: this.state.todos.filter(todo => this.state.todos.indexOf(todo) != index) })
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+            <ToDo key={ index } deleteTodo={ () => this.deleteTodo(index) } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
